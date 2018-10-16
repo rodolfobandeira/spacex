5d099caaca806a2fafd381842338e1092c8355c0
Jude Lawson <lawsonjdutton@gmail.com>
2018-10-15 18:32:12 -0600

    Cleans up ResourceService module. All Tests Passing. Rubocop linting passes

--- lib/resource_service.rb
+++ lib/resource_service.rb
@@ -1,12 +1,6 @@
 module ResourceService
-
-  def hello
-    puts 'hello'
-  end
-
   def retrieve_all(resource)
     data = SPACEX::BaseRequest.call_api(resource)
-    thing = data.get.body.map { |k| new(k) }
+    data.get.body.map { |k| new(k) }
   end
 end
-
