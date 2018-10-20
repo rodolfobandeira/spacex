4b92b466d2f8bfa31d732dab8cb66c5e71949ee9
Michael King <mtking1123@gmail.com>
2018-10-19 23:59:50 -0400

    replace all method with call to info

--- lib/spacex/launches.rb
+++ lib/spacex/launches.rb
@@ -1,5 +1,9 @@
 module SPACEX
   module Launches
+    def self.info(flight_number = nil)
+      SPACEX::BaseRequest.info("launches/#{flight_number}")
+    end
+
     def self.latest
       SPACEX::BaseRequest.info('launches/latest')
     end
@@ -9,11 +13,7 @@ module SPACEX
     end
 
     def self.all
-      SPACEX::BaseRequest.info('launches')
-    end
-
-    def self.info(flight_number = nil)
-      SPACEX::BaseRequest.info("launches/#{flight_number}")
+      info
     end
   end
 end
