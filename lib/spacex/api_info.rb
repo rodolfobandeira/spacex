1c77c01732443503a1c6c7c5ef4e10ac7f5a8563
rodolfobandeira <1811118+rodolfobandeira@users.noreply.github.com>
2026-03-21 15:03:21 -0400

    chore: delete all V3 lib files, specs, and v4 spec directory

--- lib/spacex/api_info.rb
+++
@@ -1,7 +0,0 @@
-module SPACEX
-  module ApiInfo
-    def self.info
-      SPACEX::BaseRequest.info('')
-    end
-  end
-end
--- spec/spacex/endpoint_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe SPACEX::BASE_URI do
-  subject { SPACEX::BASE_URI }
-
-  it 'returns the BASE URI for the SpaceX API' do
-    expect(subject).to eq 'https://api.spacexdata.com'
-  end
-end
